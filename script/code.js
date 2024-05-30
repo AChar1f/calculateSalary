8f660b7eeb0e49f8e03979477b58509bf0980d83
AChar1f <acharif41@gmail.com>
2024-05-30 16:28:19 +0200

    tweaks

--- script/code.js
+++ script/code.js
@@ -1,9 +1,29 @@
+function reveal() {
+    let x = document.querySelector('#salaryInfo');
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 
 function calcsal() {
-    let SalaryWindow = document.getElementById("SalaryWindow")
-    let sal = document.getElementById("sal").value 
-    let hrs = document.getElementById("hrs").value 
-    let ans = eval(+sal * +hrs)
-        SalaryWindow.value = (`Hi.  Your salary is R${ans}`)
+    let SalaryWindow = document.getElementById('SalaryWindow');
+    let hourlyRate = document.getElementById('hourlyRate').value;
+    let numberHours = document.getElementById('numberHours').value;
+    let employedYes = document.getElementById('yes').checked;
+    let employedNo = document.getElementById('no').checked;
 
-}
+    // if (employedNo) {
+    //     document.getElementById('hourlyRate').classList.add('hidden');
+    //     document.getElementById('numberHours').classList.add('hidden');
+    //     document.getElementById('Out').classList.add('hidden');
+    // } else {
+    //     document.getElementById('hourlyRate').classList.remove('hidden');
+    //     document.getElementById('numberHours').classList.remove('hidden');
+    //     document.getElementById('Out').classList.remove('hidden');
+
+        let ans = eval(+hourlyRate * +numberHours);
+        SalaryWindow.value = `Hi. Your salary is R${ans}`;
+    }
+
